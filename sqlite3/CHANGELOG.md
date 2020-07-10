d8177603d0476b2918cafe0be744957ccd311e50
Simon Binder <oss@simonbinder.eu>
2020-07-10 13:27:34 +0200

    Expose the ResultSet class

--- sqlite3/CHANGELOG.md
+++ sqlite3/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+- Expose the `ResultSet` class
+
 ## 0.1.0
 
 - Initial version
--- sqlite3/lib/sqlite3.dart
+++ sqlite3/lib/sqlite3.dart
@@ -4,5 +4,6 @@ library sqlite3;
 export 'src/api/database.dart';
 export 'src/api/exception.dart';
 export 'src/api/functions.dart';
+export 'src/api/result_set.dart';
 export 'src/api/sqlite3.dart';
 export 'src/api/statement.dart';
