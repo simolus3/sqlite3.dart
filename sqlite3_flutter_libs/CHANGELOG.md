42a7b328bc2742db5056dd71bce42525b7ae5e0b
Simon Binder <oss@simonbinder.eu>
2020-07-18 18:24:49 +0200

    Release version 0.1 of sqlite3_flutter_libs

--- sqlite3_flutter_libs/CHANGELOG.md
+++ sqlite3_flutter_libs/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0
+
+- Add an empty Dart library to improve the pub score
+
 ## 0.0.1
 
 - Initial release
---
+++ sqlite3_flutter_libs/lib/sqlite3_flutter_libs.dart
@@ -0,0 +1,8 @@
+/// This library is intentionally left empty.
+///
+/// The purpose of the `sqlite3_flutter_libs` package is to provide `sqlite3`
+/// native libraries when building Flutter apps for Android and iOS.
+///
+/// It intentionally contains no Dart code (and doesn't need to). To use this
+/// library, see the `sqlite3` package.
+library sqlite3_flutter_libs;
