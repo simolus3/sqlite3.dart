a985a61f6a2385418c361fd555d419a6eeb342cc
Simon Binder <oss@simonbinder.eu>
2020-11-30 16:28:09 +0100

    Also print compile-time options in version check

--- integration_tests/flutter_libs/test_driver/integration.dart
+++ integration_tests/flutter_libs/test_driver/integration.dart
@@ -1,8 +1,14 @@
+import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:test/test.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3/src/ffi/ffi.dart' show char, Utf8Utils, PointerUtils;
+import 'package:sqlite3/open.dart';
+
+typedef _sqlite3_compileoption_get_native = Pointer<Uint8> Function(Int32 n);
+typedef _sqlite3_compileoption_get_dart = Pointer<Uint8> Function(int n);
 
 void main() {
   enableFlutterDriverExtension();
@@ -14,5 +20,22 @@ void main() {
 
   test('can open sqlite3', () {
     print(sqlite3.version);
+
+    final getCompileOption = open.openSqlite().lookupFunction<
+        _sqlite3_compileoption_get_native,
+        _sqlite3_compileoption_get_dart>('sqlite3_compileoption_get');
+
+    String? lastOption;
+    var i = 0;
+    do {
+      final ptr = getCompileOption(i).cast<char>();
+
+      if (!ptr.isNullPointer) {
+        lastOption = ptr.readString();
+        print('Compile-option: $lastOption');
+      }
+
+      i++;
+    } while (lastOption != null);
   });
 }
