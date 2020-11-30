5233745eaabae57bea6492608c909639cd9817b0
Simon Binder <oss@simonbinder.eu>
2020-11-30 19:08:37 +0100

    Fix infinite loop in test

--- integration_tests/flutter_libs/test_driver/integration.dart
+++ integration_tests/flutter_libs/test_driver/integration.dart
@@ -33,6 +33,8 @@ void main() {
       if (!ptr.isNullPointer) {
         lastOption = ptr.readString();
         print('Compile-option: $lastOption');
+      } else {
+        lastOption = null;
       }
 
       i++;
