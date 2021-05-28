72136f35d446f7144ea1d914c0ed9f8359432dec
Simon Binder <oss@simonbinder.eu>
2021-05-28 20:50:47 +0200

    Add missing files for macOS tests

---
+++ integration_tests/flutter_libs/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import sqlite3_flutter_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+}
