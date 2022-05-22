75dd6b376c3292dd0d2b5a5349b80b736a786b28
Simon Binder <oss@simonbinder.eu>
2022-05-22 16:06:23 +0200

    Rollback changes to example

--- sqlite3/example/web/main.dart
+++ sqlite3/example/web/main.dart
@@ -2,53 +2,29 @@ import 'package:http/http.dart' as http;
 import 'package:sqlite3/wasm.dart';
 
 Future<void> main() async {
-  final fileSystem = await IndexedDbFileSystem.open(dbName: 'test');
+  final fs = await IndexedDbFileSystem.open(dbName: 'test');
+  print('loaded fs');
+
   final response = await http.get(Uri.parse('sqlite3.wasm'));
-  final sqlite3 = await WasmSqlite3.load(
-      response.bodyBytes, SqliteEnvironment(fileSystem: fileSystem));
-  final database = sqlite3.open('test');
-  print('${database.userVersion} (should be 0)');
-  database.userVersion = 1;
-  print('${database.userVersion} (should be 1)');
+  final sqlite = await WasmSqlite3.load(
+      response.bodyBytes, SqliteEnvironment(fileSystem: fs));
 
-  database.execute('CREATE TABLE IF NOT EXISTS users ( '
-      'id INTEGER NOT NULL, '
-      'name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, '
-      'password TEXT NOT NULL, '
-      'user_id INTEGER NOT NULL, '
-      'currentCompanyId INTEGER NULL REFERENCES companies (id), '
-      'PRIMARY KEY (id));');
+  print('Version of sqlite used is ${sqlite.version}');
 
-  final prepared = database.prepare('INSERT INTO users '
-      '(id, name, email, password, user_id) VALUES (?, ?, ?, ?, ?)');
+  print('opening a persistent database');
+  var db = sqlite.open('test.db');
 
-  for (var i = 0; i < 200; i++) {
-    prepared.execute(
-      [
-        BigInt.from(i),
-        'name',
-        'email${BigInt.from(i)}',
-        'password',
-        BigInt.from(i),
-      ],
-    );
+  if (db.userVersion == 0) {
+    db
+      ..execute('CREATE TABLE foo (x TEXT);')
+      ..execute("INSERT INTO foo VALUES ('foo'), ('bar');")
+      ..userVersion = 1;
   }
 
-  database.select('SELECT * FROM users').forEach((element) {
-    print(element.values);
-  });
+  print(db.select('SELECT * FROM foo'));
+  await fs.flush();
 
-  database.dispose();
-  //await fileSystem.close();
-  await Future<void>.delayed(const Duration(milliseconds: 5000));
-
-  final fileSystem2 = await IndexedDbFileSystem.open(dbName: 'test');
-  final sqlite32 = await WasmSqlite3.load(
-      response.bodyBytes, SqliteEnvironment(fileSystem: fileSystem2));
-  final database2 = sqlite32.open('test');
-  print('${database2.userVersion} (should be 2)');
-
-  database2.select('SELECT * FROM users').forEach((element) {
-    print(element.values);
-  });
+  print('re-opening database');
+  db = sqlite.open('test.db');
+  print(db.select('SELECT * FROM foo'));
 }
