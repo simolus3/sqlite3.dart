ddf9730ec03e961d111da2d7ff2808874a231444
Fabian Freund <github@fabi.online>
2022-08-24 16:25:53 +0300

    restructure exception creation;

--- sqlite3/lib/src/ffi/impl/exception.dart
+++ sqlite3/lib/src/ffi/impl/exception.dart
@@ -3,20 +3,24 @@ part of 'implementation.dart';
 SqliteException createExceptionRaw(
     Bindings bindings, Pointer<sqlite3> db, int returnCode,
     [String? previousStatement]) {
+// Getting hold of more explanatory error code as SQLITE_IOERR error group
+  // has an extensive list of extended error codes
+  final extendedCode = bindings.sqlite3_extended_errcode(db);
+  return createExceptionFromExtendedCode(
+      bindings, db, returnCode, extendedCode);
+}
+
+SqliteException createExceptionFromExtendedCode(Bindings bindings,
+    Pointer<sqlite3> db, int returnCode, int extendedErrorCode,
+    [String? previousStatement]) {
   // We don't need to free the pointer returned by sqlite3_errmsg: "Memory to
   // hold the error message string is managed internally. The application does
   // not need to worry about freeing the result."
   // https://www.sqlite.org/c3ref/errcode.html
   final dbMessage = bindings.sqlite3_errmsg(db).readString();
 
-  String explanation;
-
-  // Getting hold of more explanatory error code as SQLITE_IOERR error group
-  // has an extensive list of extended error codes
-  final extendedCode = bindings.sqlite3_extended_errcode(db);
-  final errStr = bindings.sqlite3_errstr(extendedCode).readString();
-
-  explanation = '$errStr (code $extendedCode)';
+  final errStr = bindings.sqlite3_errstr(extendedErrorCode).readString();
+  final explanation = '$errStr (code $extendedErrorCode)';
 
   return SqliteException(returnCode, dbMessage, explanation, previousStatement);
 }
