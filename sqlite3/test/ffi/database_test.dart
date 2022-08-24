b5e6dde4a0ad79d41301c6c981842be0ea5a93ac
Fabian Freund <github@fabi.online>
2022-08-24 16:27:39 +0300

    tests for backup and restore

--- sqlite3/test/ffi/database_test.dart
+++ sqlite3/test/ffi/database_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3/src/ffi/impl/implementation.dart';
 import 'package:test/test.dart';
 
 /// Additional tests to `common_database_test.dart` that aren't supported on
@@ -57,5 +58,128 @@ void main() {
     expect(db.userVersion, 1);
 
     db.dispose();
+  });
+
+  group('backup', () {
+    final path = join('.dart_tool', 'sqlite3', 'test', 'test.db');
+
+    setUp(() {
+      Directory(dirname(path)).createSync(recursive: true);
+
+      if (File(path).existsSync()) {
+        File(path).deleteSync();
+      }
+    });
+
+    tearDown(() {
+      if (File(path).existsSync()) {
+        File(path).deleteSync();
+      }
+    });
+
+    test('detects if is in-memory database', () {
+      final db1 = sqlite3.open(path) as DatabaseImpl;
+      final db2 = sqlite3.openInMemory() as DatabaseImpl;
+
+      expect(db1.isInMemory(), isFalse);
+      expect(db2.isInMemory(), isTrue);
+
+      db1.dispose();
+      db2.dispose();
+    });
+
+    test('copy in-memory', () {
+      final db1 = sqlite3.openInMemory();
+      db1.execute('CREATE TABLE a(b INTEGER);');
+      db1.execute('INSERT INTO a VALUES (1);');
+
+      //Should not be included in copy
+      final db2 = sqlite3.copyIntoMemory(db1);
+
+      db1.execute('INSERT INTO a VALUES (2);');
+
+      expect(db2.select('SELECT * FROM a'), hasLength(1));
+      expect(db1.select('SELECT * FROM a'), hasLength(2));
+
+      db1.dispose();
+      db2.dispose();
+    });
+
+    test('restore from disk into memory', () {
+      final db1 = sqlite3.open(path);
+      db1.execute('CREATE TABLE a(b INTEGER);');
+      db1.execute('INSERT INTO a VALUES (1);');
+
+      final db2 = sqlite3.copyIntoMemory(db1);
+
+      //Should not be included in copy
+      db1.execute('INSERT INTO a VALUES (2);');
+
+      expect(db2.select('SELECT * FROM a'), hasLength(1));
+      expect(db1.select('SELECT * FROM a'), hasLength(2));
+
+      db1.dispose();
+      db2.dispose();
+    });
+
+    test('backup memory to disk', () {
+      final db1 = sqlite3.openInMemory();
+      db1.execute('CREATE TABLE a(b INTEGER);');
+      db1.execute('INSERT INTO a VALUES (1);');
+
+      final db2 = sqlite3.open(path);
+
+      final progress = <double>[];
+      db1.backup(db2, progressCallback: progress.add);
+      expect(progress, containsAllInOrder(<double>[0, 100]));
+
+      //Should not be included in backup
+      db1.execute('INSERT INTO a VALUES (2);');
+
+      db1.dispose();
+      db2.dispose();
+
+      final db3 = sqlite3.open(path);
+
+      expect(db3.select('SELECT * FROM a'), hasLength(1));
+
+      db3.dispose();
+    });
+
+    test('backup disk to disk', () {
+      final pathFrom = join('.dart_tool', 'sqlite3', 'test', 'test_from.db');
+      Directory(dirname(pathFrom)).createSync(recursive: true);
+
+      if (File(pathFrom).existsSync()) {
+        File(pathFrom).deleteSync();
+      }
+
+      final db1 = sqlite3.open(pathFrom);
+
+      db1.execute('CREATE TABLE a(b INTEGER);');
+      db1.execute('INSERT INTO a VALUES (1);');
+
+      final db2 = sqlite3.open(path);
+
+      final progress = <double>[];
+      db1.backup(db2, progressCallback: progress.add);
+      expect(progress, containsAllInOrder(<double>[100]));
+
+      //Should not be included in backup
+      db1.execute('INSERT INTO a VALUES (2);');
+
+      db1.dispose();
+      db2.dispose();
+
+      final db3 = sqlite3.open(path);
+
+      expect(db3.select('SELECT * FROM a'), hasLength(1));
+
+      db3.dispose();
+
+      if (File(pathFrom).existsSync()) {
+        File(pathFrom).deleteSync();
+      }
+    });
   });
 }
