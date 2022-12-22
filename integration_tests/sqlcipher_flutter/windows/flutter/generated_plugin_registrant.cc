fcacf5123229f7c20d8afb77e79c70567705fad0
Simon Binder <oss@simonbinder.eu>
2022-12-22 13:57:55 +0100

    Add windows dir for sqlcipher integration test

---
+++ integration_tests/sqlcipher_flutter/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <sqlcipher_flutter_libs/sqlite3_flutter_libs_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Sqlite3FlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
+}
