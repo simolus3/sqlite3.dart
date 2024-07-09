043c56c2e4b2ceed2e0a8f5cb7ecfd185313e9b9
Mugi Khan <mugikhan@gmail.com>
2024-07-09 15:35:57 +0200

    Compile powersync core wasm

--- sqlite3/assets/wasm/os_web.c
+++ sqlite3/assets/wasm/os_web.c
@@ -5,6 +5,14 @@
 #include "bridge.h"
 #include "sqlite3.h"
 
-int sqlite3_os_init(void) { return SQLITE_OK; }
+extern int __rust_no_alloc_shim_is_unstable = 0;
+extern int sqlite3_powersync_init(sqlite3 *db, char **pzErrMsg,
+                                  const sqlite3_api_routines *pApi);
+
+int sqlite3_os_init(void)
+{
+    sqlite3_auto_extension((void (*)(void)) & sqlite3_powersync_init);
+    return SQLITE_OK;
+}
 
 int sqlite3_os_end(void) { return SQLITE_OK; }
