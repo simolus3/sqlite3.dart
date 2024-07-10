ed9c3b246cf22f49f4974cd98cf25263660aba0a
Mugi Khan <mugikhan@gmail.com>
2024-07-10 11:16:43 +0200

    Fix clang formatting

--- sqlite3/assets/wasm/os_web.c
+++ sqlite3/assets/wasm/os_web.c
@@ -9,10 +9,9 @@ extern int __rust_no_alloc_shim_is_unstable = 0;
 extern int sqlite3_powersync_init(sqlite3 *db, char **pzErrMsg,
                                   const sqlite3_api_routines *pApi);
 
-int sqlite3_os_init(void)
-{
-    sqlite3_auto_extension((void (*)(void)) & sqlite3_powersync_init);
-    return SQLITE_OK;
+int sqlite3_os_init(void) {
+  sqlite3_auto_extension((void (*)(void)) & sqlite3_powersync_init);
+  return SQLITE_OK;
 }
 
 int sqlite3_os_end(void) { return SQLITE_OK; }
