be9a719cb74b42e383230b3fab8c5e107dc31557
Simon Binder <oss@simonbinder.eu>
2025-01-10 23:03:10 +0100

    Reformat native code

--- sqlite3/assets/wasm/getentropy.c
+++ sqlite3/assets/wasm/getentropy.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+
 #include "bridge.h"
 
-// sqlite3mc calls getentropy on initialization. That call pulls a bunch of WASI imports in when
-// using the default WASI libc, which we're trying to avoid here.
-// Instead, we use a local implementation backed by `Random.secure()` in Dart.
+// sqlite3mc calls getentropy on initialization. That call pulls a bunch of WASI
+// imports in when using the default WASI libc, which we're trying to avoid
+// here. Instead, we use a local implementation backed by `Random.secure()` in
+// Dart.
 int getentropy(void* buf, size_t n) {
-    return xRandomness(-1, (int) n, (char*) buf);
+  return xRandomness(-1, (int)n, (char*)buf);
 }
