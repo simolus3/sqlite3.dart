132b821b9bba1b951b1e47c6a7ba4f48f887e0b6
Simon Binder <oss@simonbinder.eu>
2025-01-10 22:51:18 +0100

    Keep compiling regular sqlite3

---
+++ sqlite3/assets/wasm/getentropy.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "bridge.h"
+
+// sqlite3mc calls getentropy on initialization. That call pulls a bunch of WASI imports in when
+// using the default WASI libc, which we're trying to avoid here.
+// Instead, we use a local implementation backed by `Random.secure()` in Dart.
+int getentropy(void* buf, size_t n) {
+    return xRandomness(-1, (int) n, (char*) buf);
+}
