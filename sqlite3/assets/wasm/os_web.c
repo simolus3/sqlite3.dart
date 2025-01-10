b40a72b3942d26d561195efb032d8d1f53d93006
Simon Binder <oss@simonbinder.eu>
2025-01-10 22:51:18 +0100

    Keep compiling regular sqlite3

--- sqlite3/assets/wasm/os_web.c
+++ sqlite3/assets/wasm/os_web.c
@@ -2,18 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "bridge.h"
 #include "sqlite3.h"
 
-extern int sqlite3_powersync_init(sqlite3 *db, char **pzErrMsg,
-                                  const sqlite3_api_routines *pApi);
-
-int getentropy(void* buf, size_t n) {
-    return xRandomness(-1, (int) n, (char*) buf);
-}
-
-int sqlite3_os_init(void) {
-    return SQLITE_OK;
-}
+int sqlite3_os_init(void) { return SQLITE_OK; }
 
 int sqlite3_os_end(void) { return SQLITE_OK; }
