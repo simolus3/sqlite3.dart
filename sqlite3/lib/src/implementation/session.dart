278fa808925a59b2eef88a311060a8a79a3be648
Rody Davis <rody.davis.jr@gmail.com>
2025-03-12 21:48:32 -0700

    refactor session extension to match node.js api and also full ffi bindings

--- sqlite3/lib/src/implementation/session.dart
+++ sqlite3/lib/src/implementation/session.dart
@@ -4,13 +4,6 @@ import '../../common.dart';
 import '../session.dart';
 import 'bindings.dart';
 
-base class ChangesetIteratorImplementation implements CommonChangesetIterator {
-  final RawSqliteBindings bindings;
-  final RawChangesetIterator iterator;
-
-  ChangesetIteratorImplementation(this.bindings, this.iterator);
-}
-
 base class SessionImplementation implements CommonSession {
   final RawSqliteBindings bindings;
   final RawSqliteSession session;
@@ -18,25 +11,80 @@ base class SessionImplementation implements CommonSession {
   SessionImplementation(this.bindings, this.session);
 
   @override
-  void attach([String? name]) {
-    final result = bindings.sqlite3session_attach(session, name);
-    if (result != SqlError.SQLITE_OK) {
-      throw SqliteException(result, 'Could not attach session');
-    }
-  }
-
-  @override
   Uint8List changeset() {
-    return bindings.sqlite3session_changeset(session);
+    final result = session.sqlite3session_changeset();
+    if (result.resultCode != SqlError.SQLITE_OK) {
+      throw SqliteException(result.resultCode, 'Could not get changeset');
+    }
+    return result.result;
   }
 
   @override
   Uint8List patchset() {
-    return bindings.sqlite3session_patchset(session);
+    final result = session.sqlite3session_patchset();
+    if (result.resultCode != SqlError.SQLITE_OK) {
+      throw SqliteException(result.resultCode, 'Could not get patchset');
+    }
+    return result.result;
   }
 
   @override
-  void delete() {
-    bindings.sqlite3session_delete(session);
+  void close() {
+    session.sqlite3session_delete();
+  }
+
+  @override
+  void diff(String fromDb, String table) {
+    final result = session.sqlite3session_diff(fromDb, table);
+    if (result != SqlError.SQLITE_OK) {
+      throw SqliteException(result, 'Could not diff');
+    }
+  }
+
+  @override
+  void enable() {
+    final result = session.sqlite3session_enable(1);
+    if (result != SqlError.SQLITE_OK) {
+      throw SqliteException(result, 'Could not enable session');
+    }
+  }
+
+  @override
+  void disable() {
+    final result = session.sqlite3session_enable(0);
+    if (result != SqlError.SQLITE_OK) {
+      throw SqliteException(result, 'Could not disable session');
+    }
+  }
+
+  @override
+  bool isEnabled() {
+    return session.sqlite3session_enable(-1) == 1;
+  }
+
+  @override
+  bool isIndirect() {
+    return session.sqlite3session_indirect(-1) == 1;
+  }
+
+  @override
+  void setIndirect(bool indirect) {
+    final result = session.sqlite3session_indirect(indirect ? 1 : 0);
+    if (result != SqlError.SQLITE_OK) {
+      throw SqliteException(result, 'Could not set indirect');
+    }
+  }
+
+  @override
+  bool isEmpty() {
+    return session.sqlite3session_isempty() == 1;
+  }
+
+  @override
+  void attach([String? table]) {
+    final result = session.sqlite3session_attach(table);
+    if (result != SqlError.SQLITE_OK) {
+      throw SqliteException(result, 'Could not attach');
+    }
   }
 }
