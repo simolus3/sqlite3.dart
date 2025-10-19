b0adfea460c9bbe436ded82b18ea243f674d32ac
Simon Binder <oss@simonbinder.eu>
2025-10-19 20:33:52 +0200

    Hook overrides for system sqlite

--- tool/hook_overrides.dart
+++ tool/hook_overrides.dart
@@ -2,15 +2,26 @@ import 'dart:io';
 
 /// Adds a `hooks` section to the `pubspec.yaml` of the directory the script is
 /// running in to customize how `package:sqlite3` loads `libsqlite3`.
-void main() async {
+void main(List<String> args) async {
   final out = await File('pubspec.yaml').openWrite(mode: FileMode.append);
-  out.write('''
+  final [mode] = args;
+  switch (mode) {
+    case 'system':
+      out.write('''
+hooks:
+  user_defines:
+    sqlite3:
+      source: system
+''');
+    case 'compiled':
+      out.write('''
 hooks:
   user_defines:
     sqlite3:
       source: test-sqlite3
       directory: sqlite/out/
 ''');
+  }
 
   await out.flush();
   await out.close();
