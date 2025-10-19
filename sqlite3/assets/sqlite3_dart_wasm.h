191eb615f3e1d623e4577da1488f7c04baa6b266
Simon Binder <oss@simonbinder.eu>
2025-10-19 20:44:54 +0200

    Fix format checks

--- sqlite3/assets/sqlite3_dart_wasm.h
+++ sqlite3/assets/sqlite3_dart_wasm.h
@@ -1,29 +1,24 @@
 #include "sqlite3.h"
 
 // Additional bindings we need for WASM interop.
-// These are implemented in sqlite3_wasm_build, this header is only used for ffigen.
+// These are implemented in sqlite3_wasm_build, this header is only used for
+// ffigen.
 
 void *dart_sqlite3_malloc(int size);
 void dart_sqlite3_free(void *ptr);
 
-
 sqlite3_vfs *dart_sqlite3_register_vfs(const char *name, int dartId,
-                                                  int makeDefault);
+                                       int makeDefault);
 
-int dart_sqlite3_create_scalar_function(sqlite3 *db,
-                                                   const char *zFunctionName,
-                                                   int nArg, int eTextRep,
-                                                   int id);
+int dart_sqlite3_create_scalar_function(sqlite3 *db, const char *zFunctionName,
+                                        int nArg, int eTextRep, int id);
 
 int dart_sqlite3_create_aggregate_function(sqlite3 *db,
-                                                      const char *zFunctionName,
-                                                      int nArg, int eTextRep,
-                                                      int id);
+                                           const char *zFunctionName, int nArg,
+                                           int eTextRep, int id);
 
-int dart_sqlite3_create_window_function(sqlite3 *db,
-                                                   const char *zFunctionName,
-                                                   int nArg, int eTextRep,
-                                                   int id);
+int dart_sqlite3_create_window_function(sqlite3 *db, const char *zFunctionName,
+                                        int nArg, int eTextRep, int id);
 
 void dart_sqlite3_updates(sqlite3 *db, int id);
 
@@ -31,11 +26,10 @@ void dart_sqlite3_commits(sqlite3 *db, int id);
 
 void dart_sqlite3_rollbacks(sqlite3 *db, int id);
 
-int dart_sqlite3_create_collation(sqlite3 *db, const char *zName,
-                                             int eTextRep, int id);
+int dart_sqlite3_create_collation(sqlite3 *db, const char *zName, int eTextRep,
+                                  int id);
 
 int dart_sqlite3_db_config_int(sqlite3 *db, int op, int arg);
 
-int dart_sqlite3changeset_apply(sqlite3 *db, int nChangeset,
-                                           void *pChangeset, void *pCtx,
-                                           int filter);
+int dart_sqlite3changeset_apply(sqlite3 *db, int nChangeset, void *pChangeset,
+                                void *pCtx, int filter);
