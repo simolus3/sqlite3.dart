049689699718ce7fecb5025edec3efc453b14e89
Simon Binder <simon@journeyapps.com>
2025-10-20 22:07:05 +0200

    Try restoring iOS integration tests

--- tool/hook_overrides.dart
+++ tool/hook_overrides.dart
@@ -1,28 +1,34 @@
 import 'dart:io';
 
+import 'package:path/path.dart' as p;
+
 /// Adds a `hooks` section to the `pubspec.yaml` of the directory the script is
 /// running in to customize how `package:sqlite3` loads `libsqlite3`.
 void main(List<String> args) async {
-  final out = await File('pubspec.yaml').openWrite(mode: FileMode.append);
-  final [mode] = args;
-  switch (mode) {
-    case 'system':
-      out.write('''
+  for (final path in ['pubspec.yaml', 'examples/pubspec.yaml']) {
+    final out = await File(path).openWrite(mode: FileMode.append);
+    final [mode] = args;
+    switch (mode) {
+      case 'system':
+        out.write('''
 hooks:
   user_defines:
     sqlite3:
       source: system
 ''');
-    case 'compiled':
-      out.write('''
+      case 'compiled':
+        final outPath = p.relative('sqlite/out', from: p.dirname(path));
+
+        out.write('''
 hooks:
   user_defines:
     sqlite3:
       source: test-sqlite3
-      directory: sqlite/out/
+      directory: $outPath/
 ''');
-  }
+    }
 
-  await out.flush();
-  await out.close();
+    await out.flush();
+    await out.close();
+  }
 }
