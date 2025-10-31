1d287a46f3208815c5a7c7c57e6222f21aaa9901
Simon Binder <oss@simonbinder.eu>
2025-10-31 23:01:15 +0100

    Test custom extension build

---
+++ sqlite3/example/custom_extension/test/extension_test.dart
@@ -0,0 +1,18 @@
+import 'package:custom_extension/sqlite_vec.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  setUpAll(() {
+    sqlite3.loadSqliteVectorExtension();
+  });
+
+  test('can use vec0', () {
+    final db = sqlite3.openInMemory();
+    addTearDown(db.dispose);
+
+    db.execute(
+      'create virtual table vec_examples using vec0(sample_embedding float[8])',
+    );
+  });
+}
