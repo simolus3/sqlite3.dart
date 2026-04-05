3b417b60ef339285bdbddb17e0b5418cfbd6c3b2
Simon Binder <oss@simonbinder.eu>
2026-04-05 23:43:43 +0200

    Explicitly add target

--- sqlite3_connection_pool/tool/build_linux_sanitizer.sh
+++ sqlite3_connection_pool/tool/build_linux_sanitizer.sh
@@ -7,9 +7,10 @@ function compile() {
     RUSTDOCFLAGS="-Zsanitizer=$sanitizer" RUSTFLAGS="-Zsanitizer=$sanitizer -Zlocation-detail=none -Zfmt-debug=none -Zunstable-options -Cpanic=immediate-abort" cargo +nightly build \
         --release \
         -Z build-std=std,panic_abort \
-        -Z build-std-features=
+        -Z build-std-features= \
+        --target x86_64-unknown-linux-gnu
 
-    cp target/release/libsqlite3_connection_pool.so target/release/libsqlite3_connection_pool.$sanitizer.san.so
+    cp target/x86_64-unknown-linux-gnu/release/libsqlite3_connection_pool.so target/release/libsqlite3_connection_pool.$sanitizer.san.so
 }
 
 compile address
