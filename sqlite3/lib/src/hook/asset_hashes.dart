b38b0d20d2323334775be7106f7706fc630a0691
David Martos <davidmartos96@gmail.com>
2026-05-21 14:28:24 +0200

    hashes

--- sqlite3/lib/src/hook/asset_hashes.dart
+++ sqlite3/lib/src/hook/asset_hashes.dart
@@ -9,15 +9,15 @@
 const String? releaseTag = 'sqlite3-sqlcipher-3.3.2';
 
 const Map<String, String> assetNameToSha256Hash = {
-  'libsqlcipher.arm.android.so': 'f2dc15c3aebbbd3e34e7321eb89686e7ba46b61e25678592e82d4c1be4e0d4da',
-  'libsqlcipher.arm64.android.so': 'ca0de9929c91722e6d066cb7d0a004e7f1d2c61d6f495a8500dff6ae7f8bf1b1',
+  'libsqlcipher.arm.android.so': '941ec977eef2d7e51fcf1005304069772612258dccd70edc2aacb829f47da138',
+  'libsqlcipher.arm64.android.so': 'af77d7d8be5336d6768b1eb81f1f5b4e84f7cefd1c74d34d4904d31282fe67d3',
   'libsqlcipher.arm64.ios.dylib': 'bd94a9cdb9a2d4a5312fbea169679aaa7d4186b73ff30e726aac04e84bf70e71',
   'libsqlcipher.arm64.ios_sim.dylib': '3a11f40c21c79e9d0cab6b084bc2764864f008b92baa8b6a330ad503816dd114',
   'libsqlcipher.arm64.macos.dylib': '2ce44e43da3f6e7ac0d0cd39bfaabc35dfe10f9e9db713ce587aa0d5446f302d',
-  'libsqlcipher.ia32.android.so': '8d05e74c9aff67c21951aabf34933be61a9e3832f949198e580ba62231b36a3a',
-  'libsqlcipher.x64.android.so': 'aabf285d4066bbb66ea02d85dbdf812917110c3b7eb94af77dc7cd76da5b79a5',
+  'libsqlcipher.ia32.android.so': '9828f82ed144de73e15c9102e01d71e1cf736dd8ef67c3f82f723b2124617c2e',
+  'libsqlcipher.x64.android.so': '15577d744643ba80f7578b47c6f1481957482faa2caf3dbadbeea99df08d066d',
   'libsqlcipher.x64.ios_sim.dylib': '2b9dc11ba52bba90c7d0d933185462c9f9ca949d498b1e690852e6fccdf32933',
-  'libsqlcipher.x64.linux.so': '7dfe264f34b599f1142cee90857da58f3d9f050d87676bcaae913e05b9915d2f',
+  'libsqlcipher.x64.linux.so': 'a8f36a48002e202402cca11d1e22de08f40af0c893559640d9c8c4c27f8ea347',
   'sqlite3.debug.wasm': '9a942f7c42247e6463725b80bafc3a3f5d1ff6a6d2d27ef1ba1b06a313bf0a2e',
   'sqlite3.wasm': 'cb6b6b3a6d6cd912ef3b95ab995714a4e91694aa9a9d1cd15542314fb44982d0',
   'sqlite3mc.wasm': '4493f8b822215a3ac50364deb0e5a0491bd6ec537c0f79ee2ef1fb02575ffbdd',
